9eaea88cd0a50b4a75638159c717def89eb8bf09
hunziqin <54088965+hunziqin@users.noreply.github.com>
2021-09-02 17:19:00 +0800

    method2.go

---
+++ method继承.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Hunmen struct {
+	name  string
+	age   int
+	phone string
+}
+
+type Student struct {
+	Hunmen
+	school string
+}
+
+type Employee struct {
+	Hunmen
+	company string
+}
+
+func (h *Hunmen) SayHi() {
+	fmt.Printf("Hi,I am %s you can call me on %s\n", h.name, h.phone)
+}
+
+func main() {
+	hunzi := Student{Hunmen{"hunzi", 19, "13132884236"}, "MIT"}
+	yang := Employee{Hunmen{"yang", 18, "18269641207"}, "goland inc"}
+
+	hunzi.SayHi()
+	yang.SayHi()
+}
